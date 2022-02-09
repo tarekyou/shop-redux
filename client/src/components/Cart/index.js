9d60a8e5d09e6f0bd95cb1b196e84405af5cdb8b
tarekyou <tarekyoussef88@hotmail.com>
2022-02-08 19:46:26 -0800

    stripe

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,16 +1,29 @@
 // import React from "react";
 import React, { useEffect } from "react";
+import { useLazyQuery } from "@apollo/client";
+
+import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
 import "./style.css";
 
-import { useStoreContext } from "../../utils/GlobalState";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+// import { useLazyQuery } from "@apollo/client";
+
+// import { useStoreContext } from "../../utils/GlobalState";
 // import { TOGGLE_CART } from "../../utils/actions";
+
+// import { useLazyQuery } from "@apollo/client";
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
     async function getCart() {
@@ -23,6 +36,14 @@ const Cart = () => {
     }
   }, [state.cart.length, dispatch]);
 
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
+
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
   }
@@ -33,6 +54,19 @@ const Cart = () => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+    getCheckout({
+      variables: { products: productIds },
+    });
   }
 
   if (!state.cartOpen) {
@@ -93,7 +127,8 @@ const Cart = () => {
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
             {Auth.loggedIn() ? (
-              <button>Checkout</button>
+              //   <button>Checkout</button>
+              <button onClick={submitCheckout}>Checkout</button>
             ) : (
               <span>(log in to check out)</span>
             )}
