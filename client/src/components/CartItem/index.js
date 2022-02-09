6b9e9fba04476d9d27a46bd9fed91e90404bb30d
tarekyou <tarekyoussef88@hotmail.com>
2022-02-08 23:08:26 -0800

    removed commented code

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 
-// import { useStoreContext } from "../../utils/GlobalState";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
-  // const [, dispatch] = useStoreContext();
-
   const dispatch = useDispatch();
 
   const removeFromCart = (item) => {
